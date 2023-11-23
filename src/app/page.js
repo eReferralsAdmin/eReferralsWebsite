8faa60150557d8eef2070f598ac76c05ed05fcc1
Majid Sheikh <majidsheikh0096@gmail.com>
2023-11-23 16:25:52 +0500

    Home page cleaned

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,5 @@ import HeroSection from "../components/HeroSection";
 import NewsletterSignup from "../components/NewsletterSignup";
 import "../styles/PostGrid.css";
 export default async function Home() {
-  return <main>Home
-    
-  </main>;
+  return <main>Home</main>;
 }
