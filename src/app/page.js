cb50953708abd14f7f564bd22d62ce8e979a9c77
Majid Sheikh <majidsheikh0096@gmail.com>
2023-11-23 16:20:30 +0500

    Blog and article page completed with Sanity integration

--- src/app/page.js
+++ src/app/page.js
@@ -2,5 +2,7 @@ import HeroSection from "../components/HeroSection";
 import NewsletterSignup from "../components/NewsletterSignup";
 import "../styles/PostGrid.css";
 export default async function Home() {
-  return <main>Home</main>;
+  return <main>Home
+    
+  </main>;
 }
