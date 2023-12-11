78f0807ff3368a11e9371b28088508d9a29f1a37
Majid Sheikh <majidsheikh0096@gmail.com>
2023-12-11 21:57:48 +0500

    API URL added to env and made few changes to deploy API

--- app/(web)/blog/page.js
+++ app/(web)/blog/page.js
@@ -1,13 +1,12 @@
 import BlogList from "../../../views/blog/BlogList";
 import styles from "./blog.module.css";
 import FeaturedPost from "../../../components/FeaturedPost";
-import { fetchPosts } from "../../../lib/fetchData";
 import NewsletterSignup from "../../../components/NewsletterSignup";
 
 export const revalidate = 10;
 
 async function getPosts() {
-  const res = await fetch("http://localhost:3000/api/posts");
+  const res = await fetch(`${process.env.API_URL}/api/posts`);
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
@@ -18,13 +17,13 @@ export default async function BlogPage() {
   const posts = await getPosts();
   return (
     <main className={styles.blogContainer}>
-      <FeaturedPost />
+      {/* <FeaturedPost />
       <div className={styles.blogPosts}>
         <h1 className={styles.blogTitle}>Recent blog posts</h1>
         <BlogList posts={posts} />
       </div>
 
-      <NewsletterSignup />
+      <NewsletterSignup /> */}
     </main>
   );
 }
