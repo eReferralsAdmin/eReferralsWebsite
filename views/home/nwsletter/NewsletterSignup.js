4ec7774d549f0c8eee7cf457110818a968a92754
Majid Sheikh <majidsheikh0096@gmail.com>
2023-12-13 13:55:58 +0500

    newsletter signup form submission code added

--- views/home/nwsletter/NewsletterSignup.js
+++ views/home/nwsletter/NewsletterSignup.js
@@ -1,9 +1,21 @@
+"use client";
 import React from "react";
 import styles from "./newsletter-signup.module.css";
 import Link from "next/link";
 import { ChevronRightIcon, EnvelopeIcon } from "@heroicons/react/24/outline";
+import { useForm } from "react-hook-form";
 
 const NewsletterSignup = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    // Yahan aap form data ko handle kar sakte hain
+    console.log(data);
+  };
   return (
     <section className={`${styles.newsletterSignup}  gradient-bg`}>
       <div className={styles.newsletterSignupContainer}>
@@ -20,16 +32,30 @@ const NewsletterSignup = () => {
             </Link>
           </div>
         </div>
-        <form action="/" method="post" className={styles.signupForm}>
+        <form onSubmit={handleSubmit(onSubmit)} className={styles.signupForm}>
           <div className={styles.inputGroup}>
-            <div className={styles.inputContainer}>
+            <div
+              className={`${styles.inputContainer} ${
+                errors.email ? "error" : ""
+              }`}
+            >
               <EnvelopeIcon className={`${styles.emailIcon} btn-icon`} />
-              <input type="email" placeholder="Enter your email address" />
+              <input
+                type="email"
+                placeholder="Enter your email address"
+                {...register("email", { required: true })}
+              />
             </div>
           </div>
           <div className={styles.formFooter}>
             <div className={styles.consent}>
-              <input type="checkbox" id="consent" name="consent" required />
+              <input
+                type="checkbox"
+                id="consent"
+                name="consent"
+                required
+                {...register("consent", { required: true })}
+              />
               <label htmlFor="consent">
                 I agree with the storage & processing of my personal data
               </label>
