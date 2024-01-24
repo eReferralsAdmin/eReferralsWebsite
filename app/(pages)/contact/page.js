4b8e7b213f7d537d5495d6a2a6285e075bd8228a
Majid Sheikh <majidsheikh0096@gmail.com>
2024-01-25 00:47:42 +0500

    contact us page updated

--- app/(pages)/contact/page.js
+++ app/(pages)/contact/page.js
@@ -5,6 +5,15 @@ import styles from "./Contact.module.css";
 import SegmentedControl from "../../../components/ui/SegmentedControl";
 import { ChevronRightIcon, MinusIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
+import Faq from "../../../components/Faq";
+
+const faqs = [
+  {
+    title: "What is Mobile app development?",
+    description:
+      "It’s the creation of digital system that are tailored for mobile devices. Now more than ever the world needs to be mobilised with their business and systems and Mobile app development is the most relevant path to take. As per any other Application Development, the process is to gather requirements, define functionalities, design, build, deliver and support these mobile apps tailored for the business purpose.",
+  },
+];
 
 const ContactUs = () => {
   const {
@@ -27,84 +36,113 @@ const ContactUs = () => {
   ];
 
   return (
-    <div className={styles.container}>
-      <div className={styles.heading}>
-        <h2>Contact us!</h2>
-        <p>We respond within 24 hours.</p>
-        <div className={styles.legalLinksHeader}>
+    <>
+      <div className={styles.container}>
+        <div className={styles.heading}>
+          <h2>Contact us!</h2>
+          <p>We respond within 24 hours.</p>
+          <div className={styles.legalLinksHeader}>
+            <Link href="#terms">Terms</Link>
+            <MinusIcon className={styles.divider} />
+            <Link href="#privacy">Privacy</Link>
+          </div>
+        </div>
+
+        <form
+          className={styles.form}
+          onSubmit={handleSubmit(onSubmit)}
+          data-netlify="true"
+          name="contact"
+        >
+          <input type="hidden" name="contact-form" value="contact" />
+          <div className={styles.segmentedControlContainer}>
+            <SegmentedControl
+              options={segmentOptions}
+              name="role"
+              activeSegment={segment}
+              onSegmentChange={setSegment}
+              paddleActive={isPaddleActive}
+            />
+            {errors.role && (
+              <p className={styles.errorMessage}>{errors.role.message}</p>
+            )}
+          </div>
+
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Name"
+            required
+            className={styles.inputField}
+            {...register("name", { required: "Name is required." })}
+          />
+          {errors.name && (
+            <p className={styles.errorMessage}>{errors.name.message}</p>
+          )}
+
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Enter your email address"
+            required
+            className={styles.inputField}
+            {...register("email", {
+              required: "Enter a valid email address.",
+              pattern: /^\S+@\S+$/i,
+            })}
+          />
+          {errors.email && (
+            <p className={styles.errorMessage}>{errors.email.message}</p>
+          )}
+
+          <textarea
+            id="message"
+            name="message"
+            placeholder="Enter your message"
+            required
+            className={`${styles.inputField} ${styles.textarea}`}
+            rows={4}
+            {...register("message", { required: "Your message is required." })}
+          ></textarea>
+          {errors.message && (
+            <p className={styles.errorMessage}>{errors.message.message}</p>
+          )}
+
+          <div className={styles.formBottom}>
+            <div className={styles.consentContainer}>
+              <input
+                {...register("consent", {
+                  required: "You must agree before submitting.",
+                })}
+                id="consent"
+                type="checkbox"
+              />
+              <label htmlFor="consent" className={styles.consentLabel}>
+                I agree with the storage & processing of my personal data
+              </label>
+            </div>
+            {errors.consent && (
+              <p className={styles.errorMessage}>{errors.consent.message}</p>
+            )}
+            <button type="submit" className={styles.sendButton}>
+              <span>Send</span>
+              <ChevronRightIcon className="chevronRightIcon" />
+            </button>
+          </div>
+        </form>
+
+        <div className={styles.legalLinksFooter}>
           <Link href="#terms">Terms</Link>
           <MinusIcon className={styles.divider} />
           <Link href="#privacy">Privacy</Link>
         </div>
       </div>
-
-      <form className={styles.form}>
-        <div className={styles.segmentedControlContainer}>
-          <SegmentedControl
-            options={segmentOptions}
-            name="role"
-            activeSegment={segment}
-            onSegmentChange={setSegment}
-            paddleActive={isPaddleActive}
-          />
-        </div>
-
-        <input
-          type="text"
-          id="name"
-          name="name"
-          placeholder="Name"
-          required
-          className={styles.inputField}
-        />
-
-        <input
-          type="email"
-          id="email"
-          name="email"
-          placeholder="Enter your email address"
-          required
-          className={styles.inputField}
-        />
-
-        <textarea
-          id="message"
-          name="message"
-          placeholder="Enter your message"
-          required
-          className={`${styles.inputField} ${styles.textarea}`}
-          rows={4}
-        ></textarea>
-
-        <div className={styles.formBottom}>
-          <div className={styles.consentContainer}>
-            <input
-              {...register("consent", {
-                required: "You must agree before submitting.",
-              })}
-              id="consent"
-              type="checkbox"
-            />
-            <label htmlFor="consent" className={styles.consentLabel}>
-              I agree with the storage & processing of my personal data
-            </label>
-          </div>
-          {errors.consent && (
-            <p className={styles.errorMessage}>{errors.consent.message}</p>
-          )}
-          <button type="submit" className={styles.sendButton}>
-            <span>Send</span>
-            <ChevronRightIcon className="chevronRightIcon" />
-          </button>
-        </div>
-      </form>
-
-      <div className={styles.legalLinksFooter}>
-        <Link href="#terms">Terms</Link>
-        <MinusIcon className={styles.divider} />
-        <Link href="#privacy">Privacy</Link>
+      <div className={styles.faqContainer}>
+        <Faq faqs={faqs} />
       </div>
-    </div>
+    </>
   );
 };
 
