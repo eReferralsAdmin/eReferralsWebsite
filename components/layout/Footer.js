25703007600a8dbdf2fca8c3e1277dc9c7ef24e4
Arsany <info@ereferrals.health>
2024-07-12 11:43:31 +1000

    updated footer - changed some wording

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -8,13 +8,12 @@ const Footer = () => {
     <footer className={styles.footer}>
       <div className={styles.footerContent}>
         <div className={styles.footerTop}>
-          <p className={styles.footerTitle}>Start with something wonderful.</p>
+          <p className={styles.footerTitle}>Questions or Concerns?</p>
           <p className={styles.description}>
-            Join the over +100 clinics, +3000 patients, and +600 professionals
-            in the digital revolution of the medical report management world.
+            Whether you have any questions or need any help,<br></br>feel free to chat to us or send us an email at<br></br><strong>info@ereferrals.health</strong>
           </p>
           <Link href={"/contact"} className={styles.chatButton}>
-            Chat to us
+            Contact us
           </Link>
         </div>
         <div className={styles.footerBottom}>
